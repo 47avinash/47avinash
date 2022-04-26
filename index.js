572865b9d9fe4052bee8bcc9f42fa6e31563ec60
itstheanurag <gauravanurag36@gmail.com>
2022-04-26 17:32:46 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://itstheanurag:gaurav9878764239@cluster0.dirde.mongodb.net/Project-1_Blogging-Site", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/controllers/authorController.js
@@ -0,0 +1,1 @@
+const authorModel = require("../models/authorModel")
