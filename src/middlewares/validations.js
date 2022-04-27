ace3e606ca6127a1faf4957d61521be68a9497a6
itstheanurag <gauravanurag36@gmail.com>
2022-04-27 10:31:52 +0530

    Author Validation done

---
+++ src/middlewares/validations.js
@@ -0,0 +1,41 @@
+const {check, validationResult} = require("express-validator")
+
+exports.validateAuthor = [check('fname')
+    .trim()
+    .not()
+    .isEmpty().withMessage('First Name is Missing')
+    .isLength({ min: 3, max: 30 })
+    .withMessage('Invalid First Name, Name must be within 3 to 30 characters long'),
+
+    check('lname')
+    .trim()
+    .not()
+    .isEmpty().withMessage('First Name is Missing')
+    .isLength({ min: 3, max: 30 })
+    .withMessage('Invalid Last Name, Name must be within 3 to 30 characters long'),
+
+    check('title')
+    .trim()
+    .not()
+    .isEmpty().withMessage('title is Missing')
+    .isLength({ min: 3, max: 30 })
+    .withMessage(' titile must be within 3 to 30 characters long'),
+
+    check('email')
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Email is Invalid'),
+
+    check('password')
+    .trim()
+    .not()
+    .isEmpty().withMessage('password is Missing')
+    .isLength({ min: 3, max: 300 })
+    .withMessage(' Password must be within 3 to 300 characters long')]
+
+    exports.validate = function (req,res,next){
+        const error = validationResult(req).array()
+        if(!error.length) return next()
+
+        res.status(400).send({status : false, msg: error[0].msg})
+    }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const authorController = require("../controllers/authorController")
+const authorController = require("../controllers/authorController");
+const { validateAuthor, validate } = require('../middlewares/validations');
 
 
-router.post("/authors", authorController.createAuthor)
+router.post("/authors", validateAuthor, validate, authorController.createAuthor)
 
 module.exports = router;
