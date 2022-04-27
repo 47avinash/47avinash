825250868c1267eb768afc781e1c61a1e08a24ee
gktkumartripathi <gtgaurav1005@gmail.com>
2022-04-27 15:39:49 +0530

    ax

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,12 +1,18 @@
 const authorModel = require("../models/authorModel")
-
+const validator = require('email-validator')
 
 const createAuthor = async function(req,res){
     try{
         const requestData = req.body
         const {email} = requestData
+          const verifyEmail =  validator.validate(email)
+      
+          if(!verifyEmail){
+          return res.status(400).send({status:false,msg:'this is not a valid email'})
+           }
 
-        if( await authorModel.findOne({email: email})){
+
+         if( await authorModel.findOne({email: email})){
             return res.status(400).send({status : false, msg : "this email is already being used, please try login-in"})
             }
 
--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -4,14 +4,24 @@ const blogModel = require("../models/blogModel")
 
 const createBlog = async function(req,res){
     try{
+         
+    
         const author =  req.body.authorId
         if(author){
             if(! await authorModel.findById(author)){
               return res.status(400).send({status: false, msg : "An Author with this Id does not exist"})
             }
         }
-        
-        const blogData = req.body
+
+        const blogData = {
+            title,
+            body,
+            authorId,
+            category,
+            isPublished : isPUblished ? isPublished : false,
+            publishedAt : isPublished ? new Date() : null
+
+        }
         if(! await blogModel.exists(blogData)){
             let blog =  await blogModel.create(blogData)
             return res.status(201).send({status : true, msg: "A new Blog has been successfully created", data : blog})
@@ -21,6 +31,20 @@ const createBlog = async function(req,res){
         catch(err){
             res.status(500).send({status : false, msg : err.message })
         } 
+}     
+
+       
+
+
+const getBlogs = async function (req,res){
+    try{
+       const requestBody = req.query
+       const {authorId, category, tags, subcategory} = requestBody
+       
+    }
+    catch(err){
+        res.status(500).send({status : false, msg : err.message })
+    } 
 }
 
 module.exports = {createBlog}
